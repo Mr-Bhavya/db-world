d98bed520b228daa37189d3abb59ab047e961d92
Bhavya Duhdia <dudhiabhavy@gmail.com>
2023-09-28 19:30:49 +0530

    Add CRUD for User, add required files for maven, add POJO for tmdb data

---
+++ src/main/java/com/db/dbworld/entities/UserCredential.java
@@ -0,0 +1,8 @@
+package com.db.dbworld.entities;
+
+public class UserCredential {
+
+    private String host;
+    private String credentials;
+
+}
---
+++ src/main/java/com/db/dbworld/services/UserService.java
@@ -0,0 +1,23 @@
+package com.db.dbworld.services;
+
+import com.db.dbworld.payloads.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<UserDto> getAllUsers();
+
+    UserDto createUser(UserDto userDto);
+
+    UserDto getUserById(String id);
+
+    UserDto updateUser(UserDto userDto, String userId);
+
+    void deleteUserById(String id);
+
+    UserDto getUserByEmail(String email);
+
+    List<UserDto> searchUser(String key);
+
+}
