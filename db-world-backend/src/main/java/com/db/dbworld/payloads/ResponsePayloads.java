57fc2dedf4e35add1a3b1d5bca9e4d1d3f6c20ce
bhavya.dudhia <bhavya.dudhia@accenture.com>
2024-10-23 11:32:16 +0530

    Revert "Node Modules: Add DOB Modal, Change in UserProfile Data and API"
    This reverts commit 82394e94a67f2f016af07a5cfc1d02283836736d.

--- db-world-backend/src/main/java/com/db/dbworld/payloads/ResponsePayloads.java
+++ db-world-backend/src/main/java/com/db/dbworld/payloads/ResponsePayloads.java
@@ -2,16 +2,12 @@ package com.db.dbworld.payloads;
 
 import com.db.dbworld.payloads.dbcinema.DBCinemaRecordsDto;
 import com.db.dbworld.payloads.pm.CredentialDto;
-import com.db.dbworld.payloads.user.UserDto;
 import com.db.dbworld.services.Impl.UserDetailImpl;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,7 +28,6 @@ public class ResponsePayloads {
             this.user.put("userId", userDetails.getUserId());
             this.user.put("email", userDetails.getUsername());
             this.user.put("name", userDetails.getFirstName() + " " + userDetails.getLastName());
-            this.user.put("dob", userDetails.getDob());
         }
 
     }
@@ -64,19 +59,8 @@ public class ResponsePayloads {
         private boolean success;
     }
 
-    @Data
-    public static class UserProfileResponse{
-        private long userId;
-        private String firstName;
-        private String lastName;
-        private int age;
-        private Date dob;
-        private String gender;
-        private Long mobileNo;
-        private String email;
-        private String password;
-        private UserDto.UserRole userRole;
-        private int noOfLogin;
+    public class userResponse{
+
     }
 
 }
