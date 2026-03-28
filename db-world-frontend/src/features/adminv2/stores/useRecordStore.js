c6d91e4ad22f41be05a809418ed0c55b5f2fce4c
bhavya.dudhia <bhavya.dudhia@accenture.com>
2026-03-28 11:14:37 +0530

    feat(adminv2): add zustand stores for user and record UI state

---
+++ db-world-frontend/src/features/adminv2/stores/useRecordStore.js
@@ -0,0 +1,36 @@
+import { create } from 'zustand';
+
+export const useRecordStore = create((set) => ({
+  viewMode:    'table',
+  setViewMode: (v) => set({ viewMode: v }),
+
+  filters: {
+    name:     '',
+    type:     '',
+    year:     '',
+    tmdbId:   '',
+    recordId: '',
+  },
+  setFilter:    (key, value) => set(s => ({ filters: { ...s.filters, [key]: value } })),
+  clearFilters: () => set({ filters: { name:'', type:'', year:'', tmdbId:'', recordId:'' }, page: 0 }),
+
+  page:        0,
+  pageSize:    25,
+  setPage:     (p) => set({ page: p }),
+  setPageSize: (s) => set({ pageSize: s, page: 0 }),
+
+  sortModel:    [],
+  setSortModel: (v) => set({ sortModel: v }),
+
+  selectedRows:    [],
+  setSelectedRows: (v) => set({ selectedRows: v }),
+  clearSelection:  () => set({ selectedRows: [] }),
+
+  drawerRecordId: null,
+  modalState:     null,
+  editRecordId:   null,
+  openDrawer:     (id) => set({ drawerRecordId: id }),
+  closeDrawer:    () => set({ drawerRecordId: null }),
+  openModal:      (type, id = null) => set({ modalState: type, editRecordId: id }),
+  closeModal:     () => set({ modalState: null, editRecordId: null }),
+}));
