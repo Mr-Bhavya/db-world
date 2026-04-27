a69db66e34f45669dd183d3b28b254811cd4a691
Bhavya Duhdia <dudhiabhavy@gmail.com>
2026-04-27 11:12:40 +0530

    modify pom.xml

--- db-world-frontend/vite.config.js
+++ db-world-frontend/vite.config.js
@@ -295,7 +295,7 @@ export default defineConfig(({ mode }) => {
     },
 
     build: {
-      outDir: 'build',
+      outDir: 'dist',
       sourcemap: false,
       rollupOptions: {
         output: {
